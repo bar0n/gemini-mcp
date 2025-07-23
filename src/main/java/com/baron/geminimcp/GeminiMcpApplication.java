029fc4cd4aa7a236209f08b33fd21295634f3927
Dmytro Baron <dbaron@isg2tech.com>
2025-07-23 09:42:47 +0300

    init

---
+++ src/main/java/com/baron/geminimcp/GeminiMcpApplication.java
@@ -0,0 +1,13 @@
+package com.baron.geminimcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baron/geminimcp/GeminiMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baron.geminimcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
